2e023fc85cc2ae150ffcc8aa3344117a65ce9b35
atcsol <atcsol@vmatc>
2024-06-05 15:02:51 +0200

    ECLIPSE V1.4
    Inyección del TC[128,1] para parar la ejecución en a los 100s del inicio

--- uah_exp_freertos_project_2024/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
+++ uah_exp_freertos_project_2024/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
@@ -16,22 +16,24 @@
 #ifdef FT_UAH_EXP_SERV_129_00XX
 
 //TODO (DONE)
-
-// Los valores que queremos de Kpx y Kpy los cambiamos en el segundo 4 segundo el LOG dado
+// Los valores que queremos de Kpx y Kpy los cambiamos en el segundo 4 debido al LOG dado
 EmuGSS_TCProgram129_2 prog_FT_00XX_step_0(UNITIME_AFTER_POWER_ON + 4,
 					"FT_UAH_EXP_step 0, Set Kpx=0.2 and Kpy=0.2", 0.2, 0.2);
 
-// Los valores que queremos de CVx y CVy los cambiamos en el segundo 8 segundo el LOG dado
+// Los valores que queremos de CVx y CVy los cambiamos en el segundo 8 debido al LOG dado
 EmuGSS_TCProgram129_1 prog_FT_00XX_step_1(UNITIME_AFTER_POWER_ON + 8,
 					"FT_UAH_EXP_step 1, Set Command Vx=-0.1 and Vy=0.3", -0.1, 0.3);
 
-// Los valores que queremos de Kpx y Kpy los cambiamos en el segundo 34 segundo el LOG dado
+// Los valores que queremos de Kpx y Kpy los cambiamos en el segundo 34 debido al LOG dado
 EmuGSS_TCProgram129_2 prog_FT_00XX_step_2(UNITIME_AFTER_POWER_ON + 34,
 					"FT_UAH_EXP_step 2, Set Kpx=0.5 and Kpy=0.5", 0.5, 0.5);
 
-// Los valores que queremos de CVx y CVy los cambiamos en el segundo 38 segundo el LOG dado
+// Los valores que queremos de CVx y CVy los cambiamos en el segundo 38 debido al LOG dado
 EmuGSS_TCProgram129_1 prog_FT_00XX_step_3(UNITIME_AFTER_POWER_ON + 38,
 					"FT_UAH_EXP_step 3, Set Command Vx=0.4 and Vy=-0.2", 0.4, -0.2);
+
+// Para terminar el código, mando un TC[128,1] para los 100s de misión especificados
+EmuGSS_TCProgram128_1 prog_FT_00XX_step_4(UNITIME_AFTER_POWER_ON + 100,"FT_UAH_EXP_step 3, Reset");
 
 
 #endif
